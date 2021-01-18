b5c75eb197aca48a735548a78c22b5d4713fd8de
4217A <51804561+4217A@users.noreply.github.com>
2021-01-18 17:51:25 -0600

    Add files via upload

--- assets/scripts/myJS.js
+++ assets/scripts/myJS.js
@@ -9,36 +9,63 @@ const c = myCanvas.getContext('2d');
 // myCanvas.height = html.clientHeight + 4;
 myCanvas.width = document.documentElement.clientWidth;
 myCanvas.height = document.documentElement.clientHeight;
-//sprites
-const mySpaceshipFile = new Image();
-mySpaceshipFile.src = "assets/img/spaceship.png";
-const myBoostFile = new Image();
-myBoostFile.src = "assets/img/boost.png";
 
-const myStarsFile = new Image();
-myStarsFile.src = "assets/img/stars.png";
-const myPlanetsFile = new Image();
-myPlanetsFile.src = "assets/img/planets.png";
-const myBackgroundFile = new Image();
-myBackgroundFile.src = "assets/img/back.png";
+// const myOldStarsFile = new Image();
+// myOldStarsFile.src = "assets/img/stars old.png";
 
+
+//Objects
 class Game {
+    constructor() {
+        this.count = 0;
+    }
+
     draw() {
+        this.count++;
         // myCanvas.width = html.clientWidth;
         // myCanvas.height = html.clientHeight + 4;
-        myCanvas.width = document.documentElement.clientWidth;
-        myCanvas.height = document.documentElement.clientHeight;
+        // myCanvas.width = document.documentElement.clientWidth;
+        // myCanvas.height = document.documentElement.clientHeight;
+
+        //update
+        myBackground.update(this.count, myCanvas.width, myCanvas.height);
+        myStars.update(this.count, myCanvas.width, myCanvas.height);
+        myPlanets.update(this.count, myCanvas.width, myCanvas.height);
+        mySpaceship.update(this.count, myCanvas.width, myCanvas.height);
+        myControl.update(this.count, myCanvas.width, myCanvas.height);
+
+        //render
         c.clearRect(0, 0, myCanvas.width, myCanvas.height);
-        myBackground.render(myBackgroundFile, myCanvas.width, myCanvas.height);
-        myStars.render(myStarsFile, myCanvas.width, myCanvas.height);
-        myPlanets.render(myPlanetsFile, myCanvas.width, myCanvas.height);
-        mySpaceship.render(mySpaceshipFile, myCanvas.width, myCanvas.height);
-        myControl.render(40, myCanvas.width, myCanvas.height);
+        myBackground.render();
+        //myText.render(myTextFile, myCanvas.width, myCanvas.height);
+        myStars.render();
+        myPlanets.render();
+        mySpaceship.render();
+        myControl.render();
     }
 }
 
 class Background {
-    constructor(speed, thrust, MIN_SPEED, MAX_SPEED) {
+    constructor(myfile, speed, thrust, MIN_SPEED, MAX_SPEED) {
+        this.fileNum = myfile.length;
+        this.fileB = 1;
+        this.fileA = 0;
+        console.log(this.fileNum); //RR
+        this.file = myfile; 
+        this.fileW = myfile[0].width;
+        this.fileH = myfile[0].height;
+
+        this.period = 1; //frame speed
+        this.sX = 0; //sourceX
+        this.sY = 0; //sourceY
+        this.sY2 = 0; //sourceY 2 for second image
+        this.sW = 0; //sourceW
+        this.sH = 0; //sourceH
+        this.dX = 0; //destinationX
+        this.dY = 0; //destinationY
+        this.dW = 0; //destinationW
+        this.dH = 0; //destinationH
+
         this.altitude = 1;
         this.speed = speed; 
         this.thrust = thrust;
@@ -56,77 +83,97 @@ class Background {
         }
     }
 
-    render(myfile, canvasW, canvasH) {
-        //receives:
-        //file
-        //canvasW, canvasH
+    update(count, canvasW, canvasH) {
+        if (count % this.period == 0) {
+            //check for reaching end of file
+            if (this.altitude > this.fileH && this.fileB < this.fileNum - 1) { 
+                //reset altitude and go to next file
+                this.altitude = 1;
+                this.fileA = this.fileB;
+                this.fileB++;
+            }
 
-        c.drawImage(
-            myfile, 
-            myfile.width - canvasW - (myfile.width - canvasW)/2, 
-            myfile.height - canvasH - (this.altitude), 
-            canvasW, 
-            canvasH, 
-            0, 
-            0, 
-            canvasW, 
-            canvasH);    
-        //calc new altitude, only add gravity if speed is greater than MIN SPEED
-        //console.log(this.speed) //RR
+            //console.log(`this.fileW ${this.fileW}`); //RR
+            this.sX = Math.floor(this.fileW - canvasW - (this.fileW - canvasW)/2);
+            this.sY = Math.floor(this.fileH - canvasH - (this.altitude));
+            this.sY2 = this.fileH + this.sY;
+            this.sW = canvasW;
+            this.sH = canvasH;
+            this.dW = canvasW;
+            this.dH = canvasH;
 
-        if (myfile.height - canvasH - this.altitude > 50) {
+            //calc new altitude, only add gravity if speed is greater than MIN SPEED
             if (this.speed > this.MIN_SPEED) {
                 this.altitude = this.altitude + this.speed;
                 this.speed = this.speed + this.gravity;
             } else {
                 this.altitude = this.altitude + this.speed;
-            }    
-        } else {
-            this.altitude = myfile.height - canvasH;
+            } 
         }
+    }
+
+    render() {
+        //imgB
+        c.drawImage(this.file[this.fileB], this.sX, this.sY2, this.sW, this.sH, this.dX, this.dY, this.dW, this.dH);
+        //imgA    
+        c.drawImage(this.file[this.fileA], this.sX, this.sY, this.sW, this.sH, this.dX, this.dY, this.dW, this.dH);    
     }
 }
 
 class Spaceship {
+    constructor(myfile) {
+        console.log(myfile); //RR
+        console.log(myfile.height); //RR
+        this.file = myfile; 
+        this.fileW = myfile.width;
+        this.fileH = myfile.height;
 
-    render(myfile, canvasW, canvasH) {
-        //receives:
-        //file
-        //canvasW, canvasH
+        this.period = 1; //frame speed
+        this.sX = 0; //sourceX
+        this.sY = 0; //sourceY
+        this.sW = this.fileW; //sourceW
+        this.sH = this.fileH; //sourceH
+        this.dX = 0; //destinationX
+        this.dY = 0; //destinationY
+        this.dW = this.fileW; //destinationW
+        this.dH = this.fileH; //destinationH
+    }
 
-    //     c.drawImage(
-    //         myfile, 
-    //         myfile.width - canvasW - (myfile.width - canvasW)/2, 
-    //         myfile.height - canvasH, 
-    //         canvasW, 
-    //         canvasH, 
-    //         0, 
-    //         0, 
-    //         canvasW, 
-    //         canvasH);
-    // }
+    update(count, canvasW, canvasH) {
+        if (count % this.period == 0) { //update
+            this.dX = Math.floor((canvasW/2) - (this.fileW/2));
+            this.dY = Math.floor(canvasH - this.fileH - (0.05 * this.fileH));
+        }        
+    }
+
+    render() {
         c.drawImage(
-            myfile, 
-            0, 
-            0, 
-            myfile.width, 
-            myfile.height, 
-            (canvasW/2) - (myfile.width/2), 
-            canvasH - myfile.height - (0.05 * myfile.height), 
-            myfile.width, 
-            myfile.height);
+            this.file, 
+            this.sX, 
+            this.sY, 
+            this.fileW, 
+            this.fileH, 
+            this.dX, 
+            this.dY, 
+            this.dW, 
+            this.dH);
     }
 }
 
 class Control {
-    constructor() {
+    constructor(r) {
+        this.r = r;
         this.x = 0;
         this.y = 0;
-        this.r = 0;
+        this.startAngl = 0;
+        this.endAngl = Math.floor(2 * Math.PI);
 
+        this.period = 1; //frame speed
         this.ANIMATE = 10;
         this.ANIMATE_SIZE = 0.95;
         this.animateCount = 0;
+        this.regR = this.r;
+        this.animateR = Math.floor(this.r * this.ANIMATE_SIZE); 
     }
 
     checkClick(clickX, clickY) {
@@ -148,43 +195,40 @@ class Control {
         }
     }
 
-    render(r, canvasW, canvasH) {
-        this.r = r;
-        this.x = canvasW/2;
-        this.y = canvasH - (2.5 * r);
-        
-        // c.fillStyle = '#231d2a';
-        c.fillStyle = '#231d2a';
-        c.beginPath();
-        
-        if (this.animateCount == 0) {
-            c.beginPath();
-            c.arc(
-                this.x, 
-                this.y, 
-                this.r, 
-                0, 
-                2 * Math.PI);
-            c.fill();    
-        } else {
-            myBoost.render(myBoostFile, myCanvas.width, myCanvas.height);
+    update(count, canvasW, canvasH) {
+        if (count % this.period == 0) { //update
+            this.x = Math.floor(canvasW/2);
+            this.y = Math.floor(canvasH - (2.5 * this.regR));  
 
-            c.arc(
-                this.x, 
-                this.y, 
-                this.r * this.ANIMATE_SIZE, 
-                0, 
-                2 * Math.PI);
-            c.fill();            
-            
-            //  && myBackground.speed + myBackground.thrust < myBackground.MAX_SPEED [add back in]
-            if (this.animateCount >= this.ANIMATE) {
-                this.animateCount = 0;
+            if (this.animateCount == 0) {
+                //regular
+                this.r = this.regR;
             } else {
-                this.animateCount++;
-            }
-            //console.log(this.animateCount); //RR
+                //boost
+                console.log('boost'); //RR
+                myBoost.update(myGame.count, myCanvas.width, myCanvas.height);
+                this.r = this.animateR;
+                //count animate time
+                if (this.animateCount >= this.ANIMATE) {
+                    this.animateCount = 0;
+                } else {
+                    this.animateCount++;
+                }
+            }            
         }
+    }
+
+    render() {
+        //render boost if boosted
+        if (this.animateCount != 0) {
+            myBoost.render();
+        }
+        //render control on top
+        c.fillStyle = '#7b28a4'; //#231d2a
+        c.beginPath();
+        c.arc(this.x, this.y, this.r, this.startAngl, this.endAngl);
+        c.fill();    
+        c.closePath();
     }
 }
 
@@ -195,21 +239,81 @@ function loop() {
 }
 
 //start
-const myGame = new Game();
-const myStars = new Background(0.5, 2.5, 1, 6);
-const myPlanets = new Background(1, 3, 2, 8);
-const myBackground = new Background(1, 3, 2, 8);
-const mySpaceship = new Spaceship();
-const myBoost = new Spaceship();
-const myControl = new Control();
-//listen for click
-myCanvas.addEventListener('click', (e) => {
-    myControl.checkClick(e.clientX, e.clientY);
-})    
+//image array
+const imageLocation = [
+ 'assets/img/stars.png', 
+ 'assets/img/planets.png', 
+ 'assets/img/back.png',
+ 'assets/img/spaceship.png',
+ 'assets/img/boost.png'
+]
+const images = [];
+const planetsLocation = [
+    'assets/img/planet1.png', 
+    'assets/img/planet2.png',
+    'assets/img/planet3.png',
+    'assets/img/planet4.png'
+]
+const planetImages = [];
+let imageCount = 0;
+//game objects
+let myGame;
+let myStars;
+let myPlanets;
+let myBackground;
+let mySpaceship;
+let myBoost;
+const myControl = new Control(40);
 
-myBackgroundFile.onload = function() {
-    console.log('loaded'); //RR
+//create objects when images are loaded
+function imagesLoaded() {
+    console.log('all loaded, build and start loop'); //RR
+    console.log(images); //RR
+    //create images
+    myGame = new Game();
+    myStars = new Background([images[0], images[0], images[0]], 1, 2, 1, 4);
+    myPlanets = new Background(planetImages, 2, 3, 2, 8);
+    myBackground = new Background([images[2], images[2], images[2]], 1, 3, 2, 8);
+    mySpaceship = new Spaceship(images[3]);
+    myBoost = new Spaceship(images[4]);
+    //listen for click
+    myCanvas.addEventListener('click', (e) => {
+        myControl.checkClick(e.clientX, e.clientY);
+    });
+    //start game loop
     loop();
 }
 
+//load all images first
+imageLocation.forEach( src => {
+    const image = new Image();
+    image.src = src;
+    images.push(image);
+    image.onload = () => {
+        console.log(`${src} image loaded`); //RR
+        imageCount++; 
+        if (imageCount == imageLocation.length) {
+            //all images are loaded
+            console.log('all images are loaded now'); //RR
+            // imagesLoaded();
+        }
+    }
+});
 
+imageCount = 0;
+
+planetsLocation.forEach( src => {
+    const image = new Image();
+    image.src = src;
+    planetImages.push(image);
+    image.onload = () => {
+        console.log(`${src} image loaded`); //RR
+        imageCount++; 
+        if (imageCount == imageLocation.length) {
+            //all images are loaded
+            console.log(planetImages); //RR
+            console.log('all planet images are loaded now'); //RR
+            imagesLoaded();
+        }
+    }
+});
